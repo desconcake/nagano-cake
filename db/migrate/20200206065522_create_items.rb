5b3977990291d0110ea1774753c65f5cd72d2d73
656Krona <ykaskrmr@gmail.com>
2020-02-06 10:08:02 +0000

    修正点反映

--- db/migrate/20200206065522_create_items.rb
+++ db/migrate/20200206065522_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :item_image_id
-      t.string :non_taxed_price
-      t.string :description
+      t.string :name, null: false
+      t.string :item_image_id, null: false
+      t.string :non_taxed_price, null: false
+      t.string :description, null: false
       t.boolean :sale_status, null: false, default: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2020_02_06_080016) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "customer_id"
-    t.integer "item_id"
-    t.integer "quantity"
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,10 +45,10 @@ ActiveRecord::Schema.define(version: 2020_02_06_080016) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "item_image_id"
-    t.string "non_taxed_price"
-    t.string "description"
+    t.string "name", null: false
+    t.string "item_image_id", null: false
+    t.string "non_taxed_price", null: false
+    t.string "description", null: false
     t.boolean "sale_status", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
