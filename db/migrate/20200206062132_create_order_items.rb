ab0271d8f841a2c06fe3fe5e03b61f6079a7e6a6
2525pen <sakai3ons@gmail.com>
2020-02-06 08:10:51 +0000

    [add]oeder_items_model,enum

--- db/migrate/20200206062132_create_order_items.rb
+++ db/migrate/20200206062132_create_order_items.rb
@@ -1,10 +1,10 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
-      t.integer :order_id
-      t.integer :item_id
-      t.integer :order_quantity
-      t.string :tax_included_price
+      t.integer :order_id, null: false
+      t.integer :item_id, null: false
+      t.integer :order_quantity, null: false
+      t.string :tax_included_price, null: false
       t.integer :create_status,default: 0, null: false, limit: 1
 
       t.timestamps
