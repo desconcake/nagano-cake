43e48c457e53c65c879df68727a4d097de83e682
masaki4200 <masa326420.4200@gmail.com>
2020-02-08 09:53:33 +0000

    担当：キリ

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,7 +1,26 @@
 class Admins::GenresController < ApplicationController
+  def new
+  	@genres = Genre.new
+  end
+
   def index
+  	@genre = Genre.new
   end
 
   def edit
+  	@genre = Genre.find(params[:id])
+  end
+
+  def create
+  	@genre = Genre.new(genre_params)
+  end
+
+  def update
+  	@genre = Genre.find(params[:id])
+  	if @genre = update(book_params)
+  	else
+  		render :edit
+  	end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
+  devise_for :customers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'customers/withdraw'
 	resource :custmers
 
   namespace :admins do
-    resources 'genres/index'
-    resources 'genres/edit'
+  resources :genres, only:[ :index, :edit]
   end
   namespace :admins do
-    root 'home#top'
+  root 'home#top'
   end
 
 
@@ -23,7 +26,4 @@ end
   get 'orders/check'
   get 'orders/thanks'
 
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
