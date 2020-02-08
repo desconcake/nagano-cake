e042d829a1d8e1b68b0244f045d4df73231f0518
2525pen <sakai3ons@gmail.com>
2020-02-08 22:10:40 +0000

    [add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,20 @@ Rails.application.routes.draw do
 	  resources :genres, only:[ :index, :new, :create, :edit, :update ]
 	end
 
+  root 'home#top'
+  get 'home/about'
   resources :sub_addresses, only:[ :index, :show ]
   resources :orders, only:[ :index, :show, :new ]
   get 'orders/check'
   get 'orders/thanks'
+  resources :customers, only:[ :edit, :show, :update ]
+  get 'customers/withdraw'
+  resources :items, only:[ :index, :show ]
+  resources :cart_items, only: [ :index, :update, :create, :destroy, :clear ]
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
