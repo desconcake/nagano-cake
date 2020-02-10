02166f9a84d546fc3a2e9a2e88120371f6718927
2525pen <sakai3ons@gmail.com>
2020-02-10 06:29:57 +0000

    [add]ルーティング全件修正&追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'cart_items/index'
-  get 'cart_items/ create'
-  get 'cart_items/update'
-  get 'cart_items/destroy'
-  get 'cart_items/clear'
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ devise_for :admins
+ devise_for :customers
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#top'
-  get 'home/about'
+  get '/home/about', to: 'home#about'
   resources :customers, only:[ :edit, :show, :update ]
-  get 'customers/withdraw'
-  resources :cart_items, only: [ :index, :update, :create, :destroy, :clear ]
-  resources :orders, only:[ :index, :show, :new ]
-  get 'orders/check'
-  get 'orders/thanks'
-  resources :sub_addresses, only:[ :index, :show , :new, :create, :edit ]
+  get '/customers/withdraw', to: 'customers#withdraw'
+  resources :items, only:[ :index, :show ]
+  resources :cart_items, only: [ :index, :update, :create, :destroy ]
+  delete '/cart_items/', to: 'cart_items#clear' #カートアイテム全件削除
+  resources :orders, only:[ :index, :show, :new, :create ]
+  get 'orders/check', to: 'orders#check'
+  get 'orders/thanks', to: 'orders#thanks'
+  resources :sub_addresses, only:[ :index, :show , :new, :create, :edit, :update ]
 
   namespace :admin do
-	  get 'home/top'
+	  get '/home/top', to: 'home#about'
 	  resources :orders, only:[ :index, :show, :edit, :update ]
 	  resources :customers, only:[ :index, :show, :edit, :update ]
 	  resources :items, only:[ :index, :show, :new, :create, :edit, :update ]
