f0324fc0089a5568bf9dbf25f522e5bbafc2f528
2525pen <sakai3ons@gmail.com>
2020-02-10 03:12:42 +0000

    [fix]不具合を修正しました

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,5 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-  config.i18n.default_locale = :ja
+  # config.i18n.default_locale = :ja
 end
