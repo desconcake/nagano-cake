8fcf37db7fb7a7600d3935a2039930800abacebc
2525pen <sakai3ons@gmail.com>
2020-02-10 04:16:34 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
 
+  get 'cart_items/index'
+  get 'cart_items/ create'
+  get 'cart_items/update'
+  get 'cart_items/destroy'
+  get 'cart_items/clear'
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#top'
   get 'home/about'
-  resources :sub_addresses, only:[ :index, :show ]
-  resources :orders, only:[ :index, :show, :new ]
-  get 'orders/check'
-  get 'orders/thanks'
-
   resources :customers, only:[ :edit, :show, :update ]
   get 'customers/withdraw'
   resources :cart_items, only: [ :index, :update, :create, :destroy, :clear ]
+  resources :orders, only:[ :index, :show, :new ]
+  get 'orders/check'
+  get 'orders/thanks'
+  resources :sub_addresses, only:[ :index, :show , :new, :create, :edit ]
 
   namespace :admin do
 	  get 'home/top'
@@ -23,5 +27,4 @@ Rails.application.routes.draw do
 	  resources :genres, only:[ :index, :new, :create, :edit, :update ]
 	end
 end
-
 
