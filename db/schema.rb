00a0248a2bcdba564910d0cbd65f8c6bf1af8be8
656Krona <ykaskrmr@gmail.com>
2020-02-11 06:22:25 +0000

    price関連のデータ型変更_order_items,itemsテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_095254) do
+ActiveRecord::Schema.define(version: 2020_02_11_061011) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2020_02_08_095254) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "surname", null: false
+    t.string "name", null: false
+    t.string "surname_kana", null: false
+    t.string "name_kana", null: false
+    t.string "phone_number", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.integer "customer_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -46,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_095254) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
-    t.boolean "genre_status", null: false
+    t.boolean "genre_status", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -54,20 +62,19 @@ ActiveRecord::Schema.define(version: 2020_02_08_095254) do
   create_table "items", force: :cascade do |t|
     t.integer "genre_id", null: false
     t.string "name", null: false
-    t.string "item_image_id", null: false
-    t.string "non_taxed_price", null: false
+    t.string "item_image_id"
+    t.integer "non_taxed_price", null: false
     t.string "description", null: false
     t.boolean "sale_status", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "genre_id"
   end
 
   create_table "order_items", force: :cascade do |t|
     t.integer "order_id", null: false
     t.integer "item_id", null: false
     t.integer "order_quantity", null: false
-    t.string "tax_included_price", null: false
+    t.integer "tax_included_price", null: false
     t.integer "create_status", limit: 1, default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
