8955c84f550f8df23151600231f57a186cedb453
2525pen <sakai3ons@gmail.com>
2020-02-13 03:57:09 +0000

    [fix]

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,16 +1,35 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    item_id =
+    @cart_item.save
+  end
+
+  def edit
+    @cart_item = CartItem.find(params[:id])
   end
 
   def update
+    @cart_item.update(cart_item_params)
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
   end
 
   def clear
+    @cart_items = CartItem.all
+    @cart_items.destroy_all
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:quantity)
   end
 end
+
