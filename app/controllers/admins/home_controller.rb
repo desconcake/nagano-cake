93386105697ada77c1d1b8f00d76d32466cd6703
masaki4200 <masa326420.4200@gmail.com>
2020-02-13 07:57:47 +0000

    担当箇所のcontroller、view終わりました

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -1,4 +1,8 @@
 class Admins::HomeController < ApplicationController
   def top
+  	@orders_count = Order.where(created_at: Date.current.beginning_of_day)
+  		.order("DAY(created_at)")
+  		.count
+  		
   end
 end
