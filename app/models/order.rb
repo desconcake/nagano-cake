539a6825fc16ad66ab5a9db2c8f7247fcc03d3e6
2525pen <sakai3ons@gmail.com>
2020-02-14 16:58:15 +0000

    [fix]ステータスの表示とenum関連を修正しました

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
 	belongs_to :customer
 	has_one :customer, through: :order_item
 
-	enum status: { waiting: 0, varification: 1, production: 2, Preparing: 3, sent: 4}
+	enum status: {waiting: 0, varification: 1, production: 2, preparing: 3, sent: 4}
 	def postage
 		postage == 800
 	end
