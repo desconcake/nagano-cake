8d6829c817d2cc27b8d697182ae9818d152f753c
2525pen <sakai3ons@gmail.com>
2020-02-15 12:47:35 +0000

    [fix]

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 	belongs_to :customer
 	#has_one :customer, through: :order_item
 
-	enum status: {waiting: 0, varification: 1, production: 2, preparing: 3, sent: 4}
+	enum order_status: {waiting: 0, varification: 1, production: 2, preparing: 3, sent: 4}
 	def postage
 		postage == 800
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 
 Customer.create(id:1, surname: "山田", name: "花子", surname_kana: "ヤマダ", name_kana: "ハナコ", phone_number: "090-0000-0000", postal_code: "1500041", address: "東京都渋谷区神南１丁目１９−１１　パークスクエア２ 4階", email: "11@11", password: "1111111")
 Staff.create(email: 'admin@admin', password: 111111)
-Order.create!(id:1, :customer_id => 1, :billing_amount => 10000, :postage => 800, :method_of_payment => true, :delivery_name =>'ぺん太郎', :shipping_address => "配送先住所", :postale_code => 1600000, :order_status => 1)
+Order.create!(id:1, :customer_id => 1, :billing_amount => 10000, :postage => 800, :method_of_payment => true, :delivery_name =>'ぺん太郎', :shipping_address => "配送先住所", :postale_code => 1600000, :order_status => 'waiting')
 
 Genre.find_or_create_by(id:1, name: "ケーキ", genre_status: 1)
 Genre.find_or_create_by(id:2, name: "クッキー", genre_status: 1)
