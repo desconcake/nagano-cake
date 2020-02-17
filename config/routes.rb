7ddb7b4b9ba17b9b58f05d1ea9e042cce946e42e
656Krona <ykaskrmr@gmail.com>
2020-02-17 08:58:12 +0000

    Fix admin_staff

--- config/routes.rb
+++ config/routes.rb
@@ -24,10 +24,10 @@ Rails.application.routes.draw do
       }
 
   get 'home/about', to: 'home#about'
-  devise_for :admins, controllers: {
-        sessions: 'admins/sessions',
-        registrations: 'admins/registrations'
-      }
+  #devise_for :admins, controllers: {
+        #sessions: 'admins/sessions',
+        #registrations: 'admins/registrations'
+      #}
 
   root to: 'home#top'
   get '/home/about', to: 'home#about'
