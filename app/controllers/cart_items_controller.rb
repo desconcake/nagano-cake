9bd762bd3fdd6869f6f798d1a7b30ade0f3c8627
656Krona <ykaskrmr@gmail.com>
2020-02-19 07:37:41 +0000

    Update all

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,7 +12,7 @@ class CartItemsController < ApplicationController
     else
       @cart_item = CartItem.new(cart_item_params)
       @cart_item.item_id = params[:item_id]
-      @cart_item.customer_id = current_customer.id
+      @cart_item.customer_id = current_customer.customer_id
     end
       @cart_item.save
       redirect_to item_cart_items_path(@cart_item)
